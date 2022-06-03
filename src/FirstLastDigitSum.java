0730229b6b8372e96f0a582a0c67461d4afca501
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-06-03 11:35:19 -0700

    Challenge Solved!

--- src/FirstLastDigitSum.java
+++ src/FirstLastDigitSum.java
@@ -1,2 +1,28 @@
 public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit (int number) {
+        if (number < 0) {
+            return -1;
+        }
+        else {
+            int last =  number % 10;
+
+
+            int first = 0;
+            int tempNumber = number;
+            if (tempNumber < 10) {
+                first = tempNumber;
+            }
+            while (tempNumber >= 10) {
+                tempNumber = tempNumber / 10;
+                first = tempNumber;
+            }
+                System.out.println("first number is: " + first);
+                System.out.println("last number is: " + last);
+
+        int sum = first + last;
+
+            return sum;
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
 Write a method named sumFirstAndLastDigit with one paremeter of type int called number.
 The method needs to find the first and last digit of the parameter number passed to the method using a loop and return the sum of the first and last digit of that number.
 If the number is negative then the method needs to return -1 to indicate an invalid value.
- 
+
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(10));
 
     }
 }
